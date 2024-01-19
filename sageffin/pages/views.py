37f76bac7e259cb24c8f731e0dedce5cde715a91
AyayaPhilip <philipayaya3@gmail.com>
2024-01-19 22:31:59 +0300

    build the initial test cases

--- sageffin/pages/views.py
+++ sageffin/pages/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def HomePageView(request):
-    return HttpResponse("Hello, World!")
+# def HomePageView(request):
+#     return HttpResponse("Hello, World!")
+
+class HomePageView(TemplateView):
+    template_name = "Home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "About.html"
